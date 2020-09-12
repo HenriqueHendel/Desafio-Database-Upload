f6a7ef9b8346413084d97b8b24bc130303c0c05a
Henrique Lopes <henrique.henriquelopess@gmail.com>
2020-09-11 21:31:48 -0300

    Criando migrations e entidades para o typeorm

---
+++ src/database/migrations/1599842084526-CreateForeignKey.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+
+export default class CreateForeignKey1599842084526
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'category_id_foreign_key',
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'category_id_foreign_key');
+  }
+}
